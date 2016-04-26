426c87e00133cd9d9b8210022ec035c25e9bad07
Jason <Jcory.ng@gmail.com>
2016-04-26 14:25:17 -0400

    added comments

--- heroBattle/project/heroes/views.py
+++ heroBattle/project/heroes/views.py
@@ -23,10 +23,12 @@ def logout_view(request):
 	return redirect('heroes:index')
 
 class SignUp_View(View):
+	# made two model forms for the sign up page. One for the Django User and the other for our Leader model
 	user_form = UserSignUp
 	leader_form = LeaderSignUp
 	initial = {'key': 'value'}
 
+	# GET request that will pass the forms to the signup html
 	def get(self, request, *args, **kwargs):
 		l_signup_form = self.leader_form(initial=self.initial)
 		u_signup_form = self.user_form(initial=self.initial)
@@ -37,17 +39,23 @@ class SignUp_View(View):
 		return render(request, 'heroes/signup.html', context)
 
 	def post(self,request,*args, **kwargs):
+		# Take the form information
 		user_submit_form = self.user_form(request.POST or None)
 		leader_submit_form = self.leader_form(request.POST or None)
 		
+		# check if both are valid
 		if user_submit_form.is_valid() and leader_submit_form.is_valid():
 			
+			# cleaned data allows us to target a tag from the form through it's "name" and it will return the value
 			leader_first_name = leader_submit_form.cleaned_data['first_name']
 			leader_last_name = leader_submit_form.cleaned_data['last_name']
 			leader_bio = leader_submit_form.cleaned_data['bio']
+
+			# We have to save the user instance before we save the leader because the leader user attribute 
 			user_instance = user_submit_form.save(commit=False)
 			user_instance.save()
 
+			# create the leader instance and save it
 			leader_instance = Leader(
 				user=user_instance, 
 				first_name = leader_first_name,
@@ -56,9 +64,8 @@ class SignUp_View(View):
 				)
 
 			leader_instance.save()
-			
-
-		return redirect('heroes:index')
+			# if all this saves properly we redirect to the home page
+			return redirect('heroes:index')
 
 
 class Leader_Login(View):
